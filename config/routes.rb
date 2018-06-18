2cf000da24d7c69c1fc1f9f6088c10dc337630a2
hieulebkdn <102150274@sv.dut.edu.vn>
2018-06-18 13:03:51 +0700

    signup page and fix sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "static_pages/home"
-
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
 end
