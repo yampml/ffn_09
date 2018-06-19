297133a94a89ec294927dca29b9f85162b8377ee
NDA_ADN <nguyendinhan97@gmail.com>
2018-06-19 08:24:38 +0700

    login by google + facebook oauth + force https localhost for fb login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,23 @@
 class SessionsController < ApplicationController
-  before_action :find_user, only: :create
-
   def new; end
 
   def create
+    if params[:normal_login].present?
+      login_normal
+    else
+      login_google_oauth2
+    end
+  end
+
+  def destroy
+    logout if logged_in?
+    redirect_to root_url
+  end
+
+  private
+
+  def login_normal
+    @user = User.find_by email: params[:session][:email].downcase
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       redirect_to root_url
@@ -13,12 +27,16 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy
-    logout if logged_in?
-    redirect_to root_url
-  end
-
-  def find_user
-    @user = User.find_by email: params[:session][:email].downcase
+  def login_google_oauth2
+    auth = request.env["omniauth.auth"]
+    a_email = auth.info[:email]
+    @user = User.find_by email: a_email.downcase
+    if @user.present?
+      log_in @user
+      redirect_to @user
+    else
+      redirect_to controller: :users, action: :new, name: auth.info.name,
+        email: auth.info.email
+    end
   end
 end
