aa2a66a29868171473e90d7b3ffa50bcc16600b9
NDA_ADN <nguyendinhan97@gmail.com>
2018-06-20 14:58:52 +0700

    fix pull 08 leftover comments

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,30 +1,32 @@
 class NewsController < ApplicationController
+  before_action :force_admin, only: %i(new create)
+
   def index
-    @newss = News.paginate(page: params[:page], per_page: Settings.per_page)
+    @newss = News.newest.paginate page: params[:page], per_page: Settings.per_page
   end
 
   def show; end
 
   def new
-    if current_user.admin?
       @news = current_user.news.build
-    else
-      redirect_to root_url
-    end
   end
 
   def create
     @news = current_user.news.build news_params
     if @news.save
-      flash[:success] = I18n.t "news.create.news_created_succ_mesg"
+      flash[:success] = t "news.create.news_created_succ_mesg"
       redirect_to news_index_url
     else
-      flash[:danger] = I18n.t "news.create.news_create_failed_msg"
-      redirect_to request.referer
+      flash[:danger] = t "news.create.news_create_failed_msg"
+      render :new
     end
   end
 
   private
+
+  def force_admin
+    redirect_to root_url unless current_user.admin?
+  end
 
   def news_params
     params.require(:news).permit :header, :content
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,8 @@
 class News < ApplicationRecord
   has_many :comments, as: :commentable
   belongs_to :user
+  scope :newest, ->{order(created_at: :desc)}
+  delegate :name, to: :user, prefix: "author", allow_nil: true
 
   validates :header, presence: true
   validates :content, presence: true
