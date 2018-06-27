ec1bebf62eb815763e8e35e49401df2ddd5b9fd1
NDA_ADN <nguyendinhan97@gmail.com>
2018-06-27 10:31:23 +0700

    make crud for leagues

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,8 +1,10 @@
 class League < ApplicationRecord
-  has_many :league_team_join_model
-  has_many :teams, through: :league_team_join_model
+  has_many :league_team_join_models
+  has_many :teams, through: :league_team_join_models
   has_many :matches
   has_one :ranking
 
   validates :name, presence: true
+
+  mount_uploader :logo, LogoUploader
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,14 +2,15 @@ class Team < ApplicationRecord
   has_many :players, dependent: :destroy
   has_many :team1_matches, class_name: Match.name, foreign_key: :team1_id
   has_many :team2_matches, class_name: Match.name, foreign_key: :team2_id
-  has_many :league_team_join_model
-  has_many :leagues, through: :league_team_join_model
+  has_many :league_team_join_models
+  has_many :leagues, through: :league_team_join_models
 
   validates :name, presence: true
 
   scope :alphabet, ->{order name: :asc}
   scope :load_team_from_ids, ->(ids){where "id IN (?)", ids}
   scope :search_by_name, ->(name){where "name LIKE CONCAT('%',?,'%')", name}
+  scope :not_include, ->(teams){Team.all - teams}
   mount_uploader :picture, PictureUploader
   mount_uploader :logo, LogoUploader
 end
